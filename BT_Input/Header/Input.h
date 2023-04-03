b1af47d4a33fb1b647334292cf2e4f9d538f9742
P-X-G <105801378+P-X-G@users.noreply.github.com>
2023-04-03 07:54:47 +0700

    Delete BT_Input directory

--- BT_Input/Header/Input.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INPUT_H
-#define __INPUT_H
-
-#include <stdio.h>
-#include <stdarg.h>
-
-double tong(int sum, ...);
-
-#endif
--- BT_Input/Source/main.c
+++
@@ -1,7 +0,0 @@
-#include "Input.h"
-
-int main()
-{
-    printf("Tong: %.3f", tong(2, 7, 2, 3.2, 8, 1));
-    return 0;
-}
